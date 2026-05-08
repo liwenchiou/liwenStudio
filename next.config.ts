8cb5215d33b6f29d79780ad8a308d184d0052959
liwenchiou <aa771117@gmail.com>
2026-05-08 16:23:26 +0800

    chore: remove static export from next.config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  /* config options here */
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'eexidmcttshatqfntvga.supabase.co',
+      },
+    ],
   },
 };
 
